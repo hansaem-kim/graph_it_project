a073c4a4d236ca03d41a29e7717ac0029d99eb4f
Hansaem Kim <hansaem_kim@hotmail.com>
2021-08-15 12:39:10 -0400

    added car button

---
+++ src/scripts/car.js
@@ -0,0 +1,14 @@
+
+class Car {
+    constructor(canvas){
+        this.canvas = canvas;
+        this.ctx = canvas.getContext("2d");
+        this.dimensions = {height: canvas.height, width: canvas.width};
+        
+    }
+
+    
+
+
+
+}
