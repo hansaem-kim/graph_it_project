9a10e77ea526f20f6924d4c2807aa49b53222b52
Hansaem Kim <hansaem_kim@hotmail.com>
2021-08-18 11:56:56 -0400

    Implemented sorting for leaderboard

--- src/scripts/leader_board.js
+++ src/scripts/leader_board.js
@@ -5,17 +5,25 @@ class LeaderBoard{
     }
 
     updateBoard(score){
-        this.scores.push(score);
-        const leaderBoardDiv = document.querySelector(".leader-board-div");
+        const scoreDiv = document.querySelector(".score-div");
+
+        while (scoreDiv.firstChild){
+            scoreDiv.removeChild(scoreDiv.lastChild);
+        }
+
         const name = document.querySelector("#playername").value;
-        let tag = document.createElement("p");
-        let text = document.createTextNode(`${name}:  ${score} pts`);
-        tag.appendChild(text);
-        leaderBoardDiv.appendChild(tag);
-    }
 
-    sortBoard(){
+        this.scores.push({name: `${name}`, score: `${score}`});
 
+        function compare(a,b) {return b.score - a.score};
+        this.scores.sort(compare);
+
+        this.scores.forEach(ele => {
+            let tag = document.createElement("p");
+            let text = document.createTextNode(`${ele.name}:  ${ele.score} pts`);
+            tag.appendChild(text);
+            scoreDiv.appendChild(tag);
+        });
     }
 
 
