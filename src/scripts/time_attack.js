f81f4b820a4f5c80bbd3af73fc1bd1c67400bcef
Hansaem Kim <hansaem_kim@hotmail.com>
2021-08-18 00:36:39 -0400

    added playername box

---
+++ src/scripts/time_attack.js
@@ -0,0 +1,19 @@
+
+class TimeAttack {
+    constructor(canvas){
+        this.canvas = canvas;
+        this.ctx = canvas.getContext("2d");
+        this.dimensions = {height: canvas.height, width: canvas.width};
+        this.timeAttackMode = true;
+    }
+
+    askName(){
+        
+    }
+
+
+    
+
+}
+
+export default TimeAttack;
